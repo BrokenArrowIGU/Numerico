16ddc2880b2e9bdf9f8746520a4a28cc48226208
BrokenArrow <daniel-mhs@live.com>
2021-08-16 21:34:39 -0300

    RombergDoJhow

--- 7°Bimestre/IntegralRomberg/IntegralRomberg.py
+++ 7°Bimestre/IntegralRomberg/IntegralRomberg.py
@@ -11,56 +11,54 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 # ============================== Space for Functions ==========================
+def romberg(col1):
+
+  col1 = [item for item in col1]
+  n = len(col1)
+
+  for j in range(n - 1):
+    temp_col = [0] * (n - 1 - j)
+    for i in range(n - 1 - j):
+      power = j + 1
+      temp_col[i] = (4 ** power * col1[i + 1] - col1[i]) / (4 ** power - 1)
+    col1[:n - 1 - j] = temp_col
+    print(f'F_{j+2}',temp_col)
+  return col1[0]
+
+def trapezio(f,a, b, d):                            
+
+  n = int((b - a) / h)
+  soma = 0
+  for k in range(1, n):
+    soma += f(a + k * h)
+  return (h / 2) * (f(a) + 2 * soma + f(b))
+
 def f(x):
 
-    return 0.2+25*x-200*x**2+675*x**3-900*x**4+400**5
+    return np.exp(-x**2)
 
-def trapezio(a, b, d):                            
 
-    vs = np.linspace(a,b,d+1)
-    delta = (b - a) / d
 
-    s = 0
-    for i in range(len(vs)-1):
-        s = s + (f(vs[i])+f(vs[i+1]))*delta/2
-
-    return s
-
-def romberg(a,b,n,e):
-    s = np.zeros([n,n])
-    aux = 1
-    
-    s[0,0] = trapezio(a,b,aux)
-    
-    i = 2
-    if(i != n):
-        aux = aux*2
-        s[i,0] = trapezio(a,b,aux)
-
-    E_a = 2*e
-    k = 2
-
-    while(k <= n and E_a > e):
-        for i in range(n-k+1):
-            s[i,k] = (4**(k-1)*s[i+1,k-1]-s[i,k-1])/(4**(k-1)-1)
-        E_a = abs((s[1,k] - s[2,k-1])/(s[1,k]))
-        k = k+1
-
-    return s, E_a
 # ============================== Space for Input ==============================
-Lim_a = float(input('Limite Inferior:'))
-Lim_b = float(input('Limite Superior:'))
 
+Lim_a, Lim_b = [0, 1]
+
+h = 0.5
+k = 5
+hs = [h / 2 ** i for i in range(k)]
+col1 = [trapezio(f, Lim_a, Lim_b, hi) for hi in hs]
+print('F_1', col1)
 # ============================== Error Definition =============================
 n = 3                                                   # Número de Subdivisões
 xa = np.linspace(Lim_a,Lim_b,n)                         # Vetor de valores plot
 e = 5*10**-6                                            # Erro requerido
 E_a = float(0)
 # ============================== Main Loop/Output =============================
-I, E_a = romberg(Lim_a, Lim_b, n, e)
+r = romberg(col1)
 
-print('\nValor da Integral Trapezoidal:',I)
-print('\nErro Total:',E_a)
+
+print('\nValor da Integral Trapezoidal:',r)
+#print('\nErro Total:',E_a)
 # ============================== Space for Plots ==============================
 plt.plot(xa, f(xa), 'b')
 plt.title('F(x) = Sen (x)')
