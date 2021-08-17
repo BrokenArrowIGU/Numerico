e58ce5402f48a01f450f75ea9a8991e49bf2b88d
AntunesLeonardo <antunes.leonardo.a@gmail.com>
2021-08-16 22:09:30 -0300

    Correção e Limpeza

--- 7°Bimestre/IntegralGauss/IntegralGauss.py
+++ 7°Bimestre/IntegralGauss/IntegralGauss.py
@@ -1,15 +1,20 @@
 """
 Algorithm for solving integration through Gauss-Legendre method.
 """
+
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import integrate
 
-# Data
+Lim_inf = 0
+Lim_sup = 0.8
+pontos = 5
 
 def f(x):
     return 400*x**5-900*x**4+675*x**3-200*x**2+25*x+0.2
+    #return x + 1
 
-def CeX(p):
+"""def CeX(p):
     c_1 = np.array([2], dtype=float)
     c_2 = np.array([1, 1], dtype=c_1.dtype)
     c_3 = np.array([5/9, 8/9, 5/9], dtype=c_1.dtype)
@@ -22,21 +27,27 @@ def CeX(p):
     x_4 = np.array([-np.sqrt(525+70*np.sqrt(30))/35, -np.sqrt(525-70*np.sqrt(30))/35, np.sqrt(525-70*np.sqrt(30))/35, np.sqrt(525+70*np.sqrt(30))/35], dtype=c_1.dtype)
     x_5 = np.array([-np.sqrt(245+14*np.sqrt(70))/21, -np.sqrt(245-14*np.sqrt(70))/21, 0, np.sqrt(245-14*np.sqrt(70))/21, np.sqrt(245+14*np.sqrt(70))/21], dtype=c_1.dtype)
 
-    if p == 1: return c_1, x_1
-    elif p == 2: return c_2, x_2
-    elif p == 3: return c_3, x_3
-    elif p == 4: return c_4, x_4
-    elif p == 5: return c_5, x_5
-
+    if p == 1: return x_1, c_1
+    elif p == 2: return x_2, c_2
+    elif p == 3: return x_3, c_3
+    elif p == 4: return x_4, c_4
+    elif p == 5: return x_5, c_5
+"""
 def Intgr(Lim_a, Lim_b, p):
-    pMedio = (Lim_a +Lim_b) / 2
-    c_i, x_i = CeX(p)
+    pMedio = (Lim_a + Lim_b) / 2
+    x_i, c_i = np.polynomial.legendre.leggauss(p)
+    #x_i, c_i = CeX(p)
     I = 0
     for i in range(p):
         x_i[i] = pMedio + (Lim_b - Lim_a) * x_i[i] / 2
-        print('\n',i,'C:', c_i[i], 'Xnovo:', x_i[i], 'f(Xnovo):', f(x_i[i]))
         I += c_i[i] * f(x_i[i])
-        print('\nI:', I)
-    return I
+    return I * (Lim_b - Lim_a) / 2
 
-print(Intgr(0, 0.8, 5))
+print(Intgr(Lim_inf, Lim_sup, pontos))
+
+print(integrate.quadrature(f, Lim_inf, Lim_sup))
+
+
+xPlot = np.linspace(Lim_inf, Lim_sup, (Lim_sup - Lim_inf) * 100)
+plt.plot(xPlot, f(xPlot))
+plt.show()
