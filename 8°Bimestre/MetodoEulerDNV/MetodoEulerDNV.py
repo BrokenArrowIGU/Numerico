026a6ecaa0d5439d1e6f87632466be483577bbe9
BrokenArrowIGU <danielmqssl@gmail.com>
2021-09-05 14:33:21 -0300

    Adição da EDO de Euler

---
+++ 8°Bimestre/MetodoEulerDNV/MetodoEulerDNV.py
@@ -0,0 +1,36 @@
+#==============================================================================
+# ============================ EDO de Euler ===================================
+#################################
+# Considerations of project
+# Autor : Daniel Marques
+# Electrical Engeneering - 2021
+#################################
+#==============================================================================
+
+import numpy as np
+import time as tp
+import matplotlib.pyplot as plt
+# ============================== Space for Functions ==========================
+def dy (t,y):
+    return 4*np.exp(0.8*t)-0.5*y
+def EulerEDO(a,b,ci,h,dy):
+    yn = ci
+    for i in range(0,b,h):
+        y = yn
+        yn += dy(i,y)*h
+    return yn    
+# ============================== Space for Input ==============================
+h = 1
+a = y = y_a = 0
+b = 4
+ci = 2
+# ============================== Error Definition =============================
+
+# ============================== Main Loop/Output =============================
+y = ci
+for i in range(0,b,h):
+    y_a = y
+    y = y + dy(i,y_a)*h
+
+# ============================== Space for Plots ==============================
+print("O Resultado pelo Método de Euler é: ",round(y,4))
